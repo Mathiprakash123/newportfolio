d1bf9c08a9329ba5ec226195db3ee1748f4948ad
Mathiprakash123 <mathisivaprakash@gmail.com>
2024-04-06 21:10:35 +0530

    after navbar

--- portfolio/src/components/NavBar.jsx
+++ portfolio/src/components/NavBar.jsx
@@ -1,9 +1,45 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const NavBar = () => {
-  return <div>
-    Navbar
-  </div>;
+  return (
+    <div className="bg-black px-80 py-5 text-white flex justify-between">
+      {/* header */}
+      <div className="flex space-x-3">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="28"
+          height="28"
+          viewBox="0 0 24 24"
+          className=" fill-current "
+        >
+          <g
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="1.5"
+          >
+            <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2S2 6.477 2 12s4.477 10 10 10" />
+            <path d="M12 22c-3.314 0-6-4.477-6-10S8.686 2 12 2" />
+          </g>
+        </svg>
+        <h3 className=" pt-1 font-bold">Portfolio</h3>
+      </div>
+
+      {/* content */}
+      <div className=" space-x-6 font-semibold pt-1">
+        <Link to={"/"}>Home</Link>
+        <Link to={"/about"}>About</Link>
+        <Link to={"/skills"}>Skills</Link>
+        <Link to={"/project"}>Project</Link>
+      </div>
+
+      {/* github button */}
+      <div>
+        <a href="https://github.com/Mathiprakash123"><button className=" border border-white rounded-full px-3 mt-1">GitHub</button></a>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -35,6 +35,6 @@ const router = createBrowserRouter([
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Layout />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
