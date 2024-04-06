7205c3524abe1f105cfae8c311300f77213e0ebe
Mathiprakash123 <mathisivaprakash@gmail.com>
2024-04-06 20:34:58 +0530

    browser router

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Layout from './layout/Layout.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Layout />
   </React.StrictMode>,
 )
